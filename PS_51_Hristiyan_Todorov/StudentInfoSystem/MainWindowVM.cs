fc62fd1214f2fdfb587e8a47340579671736e964
htodorov <htodorov@BGPF1QM7CE>
2020-06-17 02:10:12 +0300

    Implement DB Connection Add ListBox control to the form to display results from db for testing Fetch data from db in the view model for testing

--- PS_51_Hristiyan_Todorov/StudentInfoSystem/MainWindowVM.cs
+++ PS_51_Hristiyan_Todorov/StudentInfoSystem/MainWindowVM.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Windows;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace StudentInfoSystem
 {
@@ -8,11 +11,13 @@ namespace StudentInfoSystem
     {
         private Student _currentStudent;
         public event PropertyChangedEventHandler PropertyChanged;
+        public List<string> StudStatusChoices { get; set; }
 
         public MainWindowVM()
         {
             StudentModel model = new StudentModel();
             StudentContent = model.GetTestStudent();
+            FillStudStatusChoices();
         }
 
         public Student StudentContent
@@ -26,6 +31,36 @@ namespace StudentInfoSystem
             if (PropertyChanged != null)
             {
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void FillStudStatusChoices()
+        {
+            StudStatusChoices = new List<string>();
+
+            using (IDbConnection connection = new SqlConnection(Properties.Settings.Default.DbConnect))
+            {
+                string sqlquery = @"SELECT StatusDescr FROM StudStatus";
+
+                IDbCommand command = new SqlCommand();
+                command.Connection = connection;
+                connection.Open();
+
+                command.CommandText = sqlquery;
+                IDataReader reader = command.ExecuteReader();
+
+                bool notEndOfResult;
+
+                notEndOfResult = reader.Read();
+
+                while (notEndOfResult)
+                {
+                    string s = reader.GetString(0);
+
+                    StudStatusChoices.Add(s);
+
+                    notEndOfResult = reader.Read();
+                }
             }
         }
     }
