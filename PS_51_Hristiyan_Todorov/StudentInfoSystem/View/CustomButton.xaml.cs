401c2a4a4a9c541a94405d7721496334790c3f9e
htodorov <htodorov@BGPF1QM7CE>
2020-06-17 21:22:25 +0300

    Add custom button implementation to utilize DependencyProperty CustomButton changes background color if the mouse is hovering over it or not

---
+++ PS_51_Hristiyan_Todorov/StudentInfoSystem/View/CustomButton.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace StudentInfoSystem.View
+{
+    /// <summary>
+    /// Interaction logic for CustomButton.xaml
+    /// </summary>
+    public partial class CustomButton : Button
+    {
+        readonly static Brush DefaultHoverBackgroundValue = new BrushConverter().ConvertFromString("#FFBEE6FD") as Brush;
+
+        public CustomButton()
+        {
+            InitializeComponent();
+        }
+
+        public Brush HoverBackground
+        {
+            get { return (Brush)GetValue(HoverBackgroundProperty); }
+            set { SetValue(HoverBackgroundProperty, value); }
+        }
+        public static readonly DependencyProperty HoverBackgroundProperty = DependencyProperty.Register(
+          "HoverBackground", typeof(Brush), typeof(CustomButton), new PropertyMetadata(DefaultHoverBackgroundValue));
+    }
+}
